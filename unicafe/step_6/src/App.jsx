ac99da587b379ecdcee32212656a30bc56458826
Shahla <kokoshahla@gmail.com>
2026-03-13 02:56:04 -0700

    Refactor App component for cleaner syntax

--- unicafe/step_6/src/App.jsx
+++ unicafe/step_6/src/App.jsx
@@ -1,19 +1,21 @@
 import { useState } from 'react'
 
-const Button = (props) => {
-  return <button onClick={props.handleClick}>{props.text}</button>
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>
 }
 
-const StatisticLine = (props) => {
-  return (
-    <p>{props.text} {props.value}</p>
+const StatisticLine = ({text, value}) =>  (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
-}
 
-const Statistics = (props) => {
-  const total = props.good + props.neutral + props.bad
-  const average = total === 0 ? 0 : (props.good - props.bad) / total
-  const positive = total === 0 ? 0 : (props.good / total) * 100
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+  const average =  (good - bad) / total
+  const positive = (100 * good) / total
 
   if (total === 0) {
     return (
@@ -26,12 +28,17 @@ const Statistics = (props) => {
 
   return (
     <div>
-      <StatisticLine text="good" value={props.good} />
-      <StatisticLine text="neutral" value={props.neutral} />
-      <StatisticLine text="bad" value={props.bad} />
-      <StatisticLine text="all" value={total} />
-      <StatisticLine text="average" value={average} />
-      <StatisticLine text="positive" value={positive + ' %'} />
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={total} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive + ' %'} />
+        </tbody>
+      </table>
     </div>
   )
 }
@@ -44,9 +51,9 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
